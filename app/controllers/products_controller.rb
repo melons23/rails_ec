8f56c30fc5a13e909dc2e69a61a8985e042f07f4
melons23 <isdom007@icloud.com>
2024-07-15 15:21:55 +0900

    商品一覧/詳細機能_修正_0715

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,17 @@
 class ProductsController < ApplicationController
   def index 
-    @products = Product.all.page(params[:page]).per(20)
+    @products = Product.with_attached_image.page(params[:page]).per(20)
   end
 
   def show
     @product = Product.find(params[:id])
 
-    @related_products = Product.limit(4).order(created_at: "DESC")
+    @related_products = Product.where.not(id: @product.id).with_attached_image.limit(4).order(created_at: "DESC")
   end
 
   private
-    def product_params
-      params.require(:product).permit(:name, :price, :description, :image)
-    end
+  
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :image)
+  end
 end
